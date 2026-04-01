988bf2e9f0e94acf352d1fd73ab637ec9f155671
Bhadra Parekh <parekh.b@northeastern.edu>
2026-04-01 06:30:33 -0400

    move supabase keys to env file, remove from source code

--- supabase.js
+++ supabase.js
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import * as SecureStore from 'expo-secure-store';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
-const SUPABASE_URL = 'https://fuqxompisecnoqrmrcau.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1cXhvbXBpc2Vjbm9xcm1yY2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMTIwNzYsImV4cCI6MjA5MDU4ODA3Nn0.q50AU8ULiBnmVhtOR2dEeDC2h2Ikxg8lBHb2bJ8fF2U';
-
-// Custom storage adapter using SecureStore
 const secureStoreAdapter = {
   getItem: async (key) => {
     return await SecureStore.getItemAsync(key);
